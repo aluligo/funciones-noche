3af4a1f40a11e6725dadb9df0fb809bb316be62d
Alexander Luligo <alexrkm11@gmail.com>
2024-10-03 18:53:00 -0500

    Se crea el proyecto con el archivo Main

---
+++ funcionesProject/Main.py
@@ -0,0 +1,16 @@
+#Ejemplo de una funcion
+def my_funtion():
+    print("Hello from a function")
+    print("Hoy es el Quiz")
+#invocar la funcion
+my_funtion()
+
+#############################
+# Definir una funcion
+#Ejemplo funcion con argumentos
+def mostrarNombre(nombre):
+    print("Su nombre es: "+nombre,)
+nom = input("Ingrese su nombre: ")
+
+#invocar la funcion
+mostrarNombre(nom)
